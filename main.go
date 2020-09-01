3118c78cd2aaef0400ad4478c16788286b10803a
Cameron Booth <cdb@github.com>
2020-09-01 16:40:25 -0700

    Test cases and basic parsing into a one-word “expression”

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	fmt.Println("hello")
 }
 
-func parse(in string) interface{} {
+func parse(in string) *expr {
 	ast := &expr{}
 
 	err := parser.Parse(strings.NewReader(in), ast)
@@ -25,14 +25,45 @@ func parse(in string) interface{} {
 }
 
 type expr struct {
-	Symbol string `@Ident`
+	Literal *Literal `@@`
+}
+
+// Literal is a "union" type, where only one matching value will be present.
+type Literal struct {
+	Pos lexer.Position
+
+	Nil   *string  `@Null`
+	Str   *string  `| @String`
+	Int   *int64   `| @Integer`
+	Float *float64 `| @Float`
+	Bool  *myBool  `| @Bool`
+}
+
+type myBool bool
+
+func (mb *myBool) Capture(values []string) error {
+	out := strings.Join(values, "") == "true"
+	*mb = myBool(out)
+	return nil
+}
+
+func (l *Literal) IsNil() bool {
+	return l.Nil != nil
+}
+
+func (l *Literal) True() bool {
+	if l.Bool != nil {
+		return *l.Bool == myBool(true)
+	}
+	return false
 }
 
 var (
 	exprLexer = lexer.Must(regex.New(`
 		Null = null
-		Bool = false|true
-		Number = -?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?
+		Bool = (false|true)
+		Float = -?(?:0|[1-9]\d*)(?:\.\d+)(?:[eE][+-]?\d+)?
+		Integer = -?\d+
 		String = '([^\\']|'')*'
 		Whitespace = \s+
 		Ident = [[:ascii:]][\w\d]*
---
+++ main_test.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/alecthomas/participle/lexer"
+	"github.com/stretchr/testify/suite"
+)
+
+type mainSuite struct {
+	suite.Suite
+}
+
+func TestMain(t *testing.T) {
+	suite.Run(t, new(mainSuite))
+}
+
+func (s *mainSuite) SetupTest() {
+}
+
+func (s *mainSuite) Test_LexingSimple() {
+	sym := exprLexer.Symbols()
+	sbr := lexer.SymbolsByRune(exprLexer)
+
+	type tokInfo struct {
+		t rune
+		v string
+	}
+	tests := []struct {
+		in  string
+		typ string
+		val string
+	}{
+		{"null", "Null", "null"},
+		{"false", "Bool", "false"},
+		{"true", "Bool", "true"},
+		{"711", "Integer", "711"},
+		{"-9.2", "Float", "-9.2"},
+		// "0xff" Worry about hex later
+		{"-2.99e-2", "Float", "-2.99e-2"},
+		{"'Mona the Octocat'", "String", "'Mona the Octocat'"},
+		{"'It''s open source!'", "String", "'It''s open source!'"},
+		{" ", "Whitespace", " "},
+	}
+
+	// fmt.Printf("---- exprLexer exprLexer.Symbols() %+v\n", exprLexer.Symbols())
+
+	for _, tc := range tests {
+		s.Run(tc.in, func() {
+			lx, err := exprLexer.Lex(strings.NewReader(tc.in))
+			s.NoError(err)
+
+			toks, err := lexer.ConsumeAll(lx)
+			s.NoError(err)
+
+			s.Len(toks, 2)
+			tok := toks[0] // Only care about the first, the second should be EOF
+
+			s.Equal(sym[tc.typ], tok.Type, fmt.Sprintf("Expected %s got %v", tc.typ, sbr[tok.Type]))
+			s.Equal(tc.val, tok.Value)
+		})
+	}
+}
+
+func (s *mainSuite) Test_ParsingSimple() {
+	type tokInfo struct {
+		t rune
+		v string
+	}
+	tests := []struct {
+		in     string
+		testFn func(*mainSuite, *Literal)
+	}{
+		{"null", func(s *mainSuite, res *Literal) {
+			s.NotNil(res.Nil)
+			s.True(res.IsNil())
+		}},
+		{"false", func(s *mainSuite, res *Literal) {
+			fmt.Printf("----  res %+v\n", res)
+			s.NotNil(res.Bool)
+			s.False(res.True())
+		}},
+		{"true", func(s *mainSuite, res *Literal) {
+			s.NotNil(res.Bool)
+			s.True(res.True())
+		}},
+		{"'qwebe'", func(s *mainSuite, res *Literal) {
+			s.NotNil(res.Str)
+			s.Equal(*res.Str, "'qwebe'")
+		}},
+		{"711", func(s *mainSuite, res *Literal) {
+			s.NotNil(res.Int)
+			s.Equal(*res.Int, int64(711))
+		}},
+		{"-9.2", func(s *mainSuite, res *Literal) {
+			s.NotNil(res.Float)
+			s.Equal(*res.Float, float64(-9.2))
+		}},
+		// // "0xff" Worry about hex later
+		{"-2.99e-2", func(s *mainSuite, res *Literal) {
+			s.NotNil(res.Float)
+			s.Equal(*res.Float, float64(-2.99e-2))
+		}},
+		{"'Mona the Octocat'", func(s *mainSuite, res *Literal) {
+			s.NotNil(res.Str)
+			s.Equal(*res.Str, "'Mona the Octocat'")
+		}},
+		{"'It''s open source!'", func(s *mainSuite, res *Literal) {
+			s.NotNil(res.Str)
+			s.Equal(*res.Str, "'It''s open source!'")
+		}},
+	}
+
+	for _, tc := range tests {
+		s.Run(tc.in, func() {
+			res := parse(tc.in)
+			tc.testFn(s, res.Literal)
+		})
+	}
+
+}
