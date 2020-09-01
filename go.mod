2f14b35e51173ef62b6c22ceeb855456f79ad702
Cameron Booth <cdb@github.com>
2020-09-01 14:51:38 -0700

    Some initial lexing into base types

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/cdb/actions-parser
 
 go 1.14
+
+require (
+	github.com/alecthomas/participle v0.6.0
+	github.com/stretchr/testify v1.6.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/alecthomas/participle"
+	"github.com/alecthomas/participle/lexer"
+	"github.com/alecthomas/participle/lexer/regex"
+)
 
 func main() {
 	fmt.Println("hello")
 }
+
+func parse(in string) interface{} {
+	ast := &expr{}
+
+	err := parser.Parse(strings.NewReader(in), ast)
+	if err != nil {
+		fmt.Printf("----  err %+v\n", err)
+
+	}
+	return ast
+}
+
+type expr struct {
+	Symbol string `@Ident`
+}
+
+var (
+	exprLexer = lexer.Must(regex.New(`
+		Null = null
+		Bool = false|true
+		Number = -?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?
+		String = '([^\\']|'')*'
+		Whitespace = \s+
+		Ident = [[:ascii:]][\w\d]*
+	  `))
+
+	parser = participle.MustBuild(&expr{},
+		participle.Lexer(exprLexer),
+	)
+)
